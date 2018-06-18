b4cbb391cd9a65270910e5ad97a461e426a580a4
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-18 08:41:41 +0100

    add routes for the about page add routes for the contact page add routes for the / page

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-  console.log(request.url)
+  if (request.url === '/about') {
+    return response.end('THE ABOUT PAGE')
+  } else if (request.url === '/contact') {
+    return response.end('THE CONTACT PAGE')
+  } else if (request.url === '/') {
+    return response.end('THE HOME PAGE')
+  } else {
+    response.writeHead(404)
 
-  response.end('HELLO NODE JS')
+    response.end('THE PAGE WAS NOT FOUND.')
+  }
 })
 
 server.listen(3000)
