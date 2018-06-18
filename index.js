7f44759163236a7b98631ec9ff3c19b499fc40bb
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-18 08:52:06 +0100

    add index.html, contact.html, and about.html pages

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const http = require('http')
+const fs = require('fs')
+
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const homePage = fs.readFileSync('index.html')
+
 
 const server = http.createServer((request, response) => {
   if (request.url === '/about') {
-    return response.end('THE ABOUT PAGE')
+    return response.end(aboutPage)
   } else if (request.url === '/contact') {
-    return response.end('THE CONTACT PAGE')
+    return response.end(contactPage)
   } else if (request.url === '/') {
-    return response.end('THE HOME PAGE')
+    return response.end(homePage)
   } else {
     response.writeHead(404)
 
