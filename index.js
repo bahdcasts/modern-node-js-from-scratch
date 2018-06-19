cc3abe9056aeacd1d544e512f3761359286e10bf
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-19 12:45:16 +0100

    add index.css, and index.js files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'index.html'))
 })
---
+++ public/js/index.js
@@ -0,0 +1,1 @@
+console.log('THE INDEX PAGE.')
